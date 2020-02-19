2569bd61393ae6d44ed257ce65d9a81eccb0e982
=rajneesh.k.anand@gmail.com <=rajneesh.k.anand@gmail.com>
2020-02-19 17:46:25 +0530

    typo error

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator');
-const fs = require('fs');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
 // db
@@ -16,52 +16,53 @@ dotenv.config();
 // mongodb+srv://kaloraat_admin:kkkkkk9@nodeapi-pbn7j.mongodb.net/nodeapi?retryWrites=truenodeAPI?retryWrites=true
 // mongodb+srv://robertchou_admin:Aeiourc2491@nodeapi-p2o93.mongodb.net/nodeapi?retryWrites=true&w=majority
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log('DB Connected'));
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true
+	})
+	.then(() => console.log("DB Connected"));
 
-mongoose.connection.on('error', err => {
-    console.log(`DB connection error: ${err.message}`);
+mongoose.connection.on("error", err => {
+	console.log(`DB connection error: ${err.message}`);
 });
 
 // bring in routes
-const postRoutes = require('./routes/post');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const newsRoutes = require('./routes/news');
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const newsRoutes = require("./routes/news");
 // apiDocs
-app.get('/api', (req, res) => {
-    fs.readFile('docs/apiDocs.json', (err, data) => {
-        if (err) {
-            res.status(400).json({
-                error: err
-            });
-        }
-        const docs = JSON.parse(data);
-        res.json(docs);
-    });
+app.get("/api", (req, res) => {
+	fs.readFile("docs/apiDocs.json", (err, data) => {
+		if (err) {
+			res.status(400).json({
+				error: err
+			});
+		}
+		const docs = JSON.parse(data);
+		res.json(docs);
+	});
 });
 
 // middleware -
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
-app.use(cors());
-app.use('/api', postRoutes);
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', newsRoutes);
+// app.use(cors());
+app.options("*", cors());
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", newsRoutes);
 app.use(function(err, req, res, next) {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).json({ error: 'Unauthorized!' });
-    }
+	if (err.name === "UnauthorizedError") {
+		res.status(401).json({ error: "Unauthorized!" });
+	}
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`A Node Js API is listening on port: ${port}`);
+	console.log(`A Node Js API is listening on port: ${port}`);
 });
