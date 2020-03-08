fb636af5b8d266b7c536afcb8d7633b3fa70db91
=rajneesh.k.anand@gmail.com <=rajneesh.k.anand@gmail.com>
2020-03-09 00:30:27 +0530

    route changed

--- app.js
+++ app.js
@@ -8,13 +8,9 @@ const expressValidator = require("express-validator");
 const fs = require("fs");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 
-// db
-// mongodb://kaloraat:dhungel8@ds257054.mlab.com:57054/nodeapi
-// MONGO_URI=mongodb://localhost/nodeapi
-// mongodb+srv://kaloraat_admin:kkkkkk9@nodeapi-pbn7j.mongodb.net/nodeapi?retryWrites=truenodeAPI?retryWrites=true
-// mongodb+srv://robertchou_admin:Aeiourc2491@nodeapi-p2o93.mongodb.net/nodeapi?retryWrites=true&w=majority
 mongoose
 	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true
@@ -25,8 +21,10 @@ mongoose.connection.on("error", err => {
 	console.log(`DB connection error: ${err.message}`);
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 const corsOptions = {
-	// origin: "http://localhost:3000"
+	//origin: "http://localhost:3000"
 	origin: "https://naukrilelo.herokuapp.com"
 };
 app.options("*", cors());
--- controllers/post.js
+++ controllers/post.js
@@ -22,6 +22,24 @@ exports.postById = (req, res, next, id) => {
 		});
 };
 
+exports.postBySlug = (req, res, next, id) => {
+	Post.findById(id)
+		.populate("postedBy", "_id name")
+		.populate("comments.postedBy", "_id name")
+		.populate("postedBy", "_id name role")
+		.populate("category", "_id name ")
+		.select("_id title body created likes comments photo")
+		.exec((err, post) => {
+			if (err || !post) {
+				return res.status(400).json({
+					error: err
+				});
+			}
+			req.post = post;
+			next();
+		});
+};
+
 exports.postByCategory = (req, res) => {
 	Post.find({ category: req.query.category })
 
